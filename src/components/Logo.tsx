c8b10e0e2e117b43009d807cabde1064db397ce5
AlexEP11 <aleeps2911@gmail.com>
2025-04-14 13:01:52 -0600

    Añade Responsividad al NavBar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
 
-export const Logo = () => {
+interface Props {
+    scrolled?: boolean;
+}
+
+export const Logo = ({ scrolled = false }: Props) => {
     return (
-        <div className="bg-white rounded-b-2xl p-2 shadow-sm max-w-[160px]">
+        <div
+            className={`bg-white rounded-b-lg p-1 ${
+                scrolled ? "shadow-none" : "shadow-sm"
+            }  max-w-[160px]`}
+        >
             <Image
                 src="/logo-vertical.png"
                 width={80}
                 height={80}
                 alt="Logo de En busca del Escritor Perdido"
-                className="w-full h-auto object-contain"
+                className="w-15 h-15 xs:w-full lg:w-auto lg:h-auto object-contain "
                 priority
             />
         </div>
---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,25 @@
+import { navItems } from "@/data";
+import Link from "next/link";
+
+interface Props {
+    scrolled?: boolean;
+}
+
+export const NavLinks = ({ scrolled = false }: Props) => {
+    return (
+        <nav className="hidden sm:flex justify-evenly items-center gap-10  ">
+            {navItems.map((item) => (
+                <Link
+                    key={item.path}
+                    href={item.path}
+                    className={`group relative ${
+                        scrolled ? "text-black" : "text-white"
+                    } font-semibold px-5 py-2 transition-all duration-300`}
+                >
+                    {item.title}
+                    <span className="absolute  left-0 -bottom-1 h-[3px] w-full scale-x-0 transform bg-gradient-to-r from-azul to-azul-bajo transition-transform duration-300 ease-in-out group-hover:scale-x-100 origin-left" />
+                </Link>
+            ))}
+        </nav>
+    );
+};
