701ae35743951057c992199b6b7fefec171f9d14
AlexEP11 <aleeps2911@gmail.com>
2025-04-14 18:27:05 -0600

    Modifica tamaños del Header en diferentes dispositivos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Header = () => {
     return (
-        <header className="relative h-[540px] sm:h-[640px] md:h-[768px] lg:h-[860px] xl:h-[960px] w-full overflow-hidden">
+        <header className="relative h-[850px] md:h-[1024px] xl:h-[1024px] w-full overflow-hidden">
             <Navbar />
             <div className="absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center ">
                 <h1 className="text-4xl md:text-6xl title-cabin uppercase text-white drop-shadow-lg">
