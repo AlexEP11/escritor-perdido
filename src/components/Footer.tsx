f368481530f29b9dd05d7fe60a87216d4b032b2b
AlexEP11 <aleeps2911@gmail.com>
2025-04-18 11:08:10 -0600

    Añade animaciones a los componentes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -56,16 +56,16 @@ export const Footer = () => {
                     <div>
                         <p className="font-semibold">Dirección:</p>
                         <p className="text-sm">
-                            Avenida Tecnológico #100 Ciudad Guzmán de, 49100,
-                            Cdad. Guzmán, Jal.
+                            C. Federico del Toro 460, Centro, 49000 Cdad.
+                            Guzmán, Jal.
                         </p>
                     </div>
                     <div className="w-full">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3757.0829277140497!2d-103.49075652405408!3d19.666467133169217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f86b69affffff%3A0x540ea0f14dc3b497!2sInstituto%20Tecnol%C3%B3gico%20de%20Ciudad%20Guzm%C3%A1n!5e0!3m2!1ses-419!2smx!4v1744940535819!5m2!1ses-419!2smx"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d938.9928258247706!2d-103.46297233037598!3d19.71384586135318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f86afd15a04df%3A0xc4a4a8d6c7df57c7!2sC.%20Federico%20del%20Toro%20460%2C%20Centro%2C%2049000%20Cdad.%20Guzm%C3%A1n%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1744992742679!5m2!1ses-419!2smx"
                             width="650"
                             height="350"
-                            className="rounded-xl max-w-xs sm:max-w-2xl md:max-w-xl lg:max-w-md 2xl:max-w-2xl  "
+                            className="rounded-xl max-w-xs sm:max-w-2xl md:max-w-xl lg:max-w-md 2xl:max-w-2xl "
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
