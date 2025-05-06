fe8e235a476f61645ee9d275106a1a1eeaf814b0
AlexEP11 <aleeps2911@gmail.com>
2025-05-05 20:53:20 -0600

    Refactorizacion archivos de interfaces

---
+++ src/interfaces/circles.interface.ts
@@ -0,0 +1,10 @@
+export interface CircleInfoI {
+    path: string;
+    color?: string;
+    title: string;
+    style?: string;
+    info: {
+        text: string;
+        lista: string[];
+    };
+}
