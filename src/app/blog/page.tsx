db231e2e8c3c5448eadea198d00a480b237d40bf
AlexEP11 <aleeps2911@gmail.com>
2025-06-03 10:38:13 -0600

    Añade paginacion y post en la pagina de blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,35 +1,5 @@
-import { BlogCard, Footer, Header } from "@/components";
-import { imagesBlog } from "@/data";
+import { redirect } from "next/navigation";
 
 export default function BlogPage() {
-    // TODO: Hacer peticion fetch para obtener todas las entradas de blog en un array
-    return (
-        <>
-            <Header
-                title="Historias, talleres y momentos para recordar"
-                button={false}
-                images={imagesBlog}
-            />
-
-            <h1 className="title-chelsea text-5xl uppercase mt-16 text-center">
-                Cada experiencia, un recuerdo{" "}
-            </h1>
-            <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 xl:max-w-[1440px] mt-20 mx-auto px-5 justify-items-center">
-                <BlogCard id={0} />
-                <BlogCard id={1} />
-                <BlogCard id={2} />
-                <BlogCard id={3} />
-                <BlogCard id={4} />
-                <BlogCard id={5} />
-                <BlogCard id={6} />
-                <BlogCard id={7} />
-                <BlogCard id={8} />
-                <BlogCard id={9} />
-                <BlogCard id={10} />
-                <BlogCard id={11} />
-            </section>
-
-            <Footer />
-        </>
-    );
+    redirect("/blog/1");
 }
